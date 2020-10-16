6742e0306007e50649dc4ba325c4296b09278fd2
Francisco de Assis Boldt <fboldt@gmail.com>
2020-10-16 09:18:00 -0300

    refator login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,61 +1,81 @@
-document.addEventListener("DOMContentLoaded", startupCheckLogin);
-
-function startupCheckLogin() {
-    createCheckLoginDiv();
-    fetchCheckLoginPage();
-}
-
-function createCheckLoginDiv() {
-    let login = document.createElement("div");
-    login.setAttribute("id", "login");
-    document.body.appendChild(login);
-}
-
-function fetchCheckLoginPage() {
-    fetch('auth/checklogin.php')
-        .then(response => response.json())
-        .then(data => treatLogin(data));
-}
-
-function treatLogin(data) {
-    if (userIsLogged(data)) {
-        document.querySelector("#login").innerHTML = `${data.username}
-        <a href="javascript:logout()">logout</a>`;
-        document.querySelector("#login").setAttribute("userid", data.userid);
-    } else {
-        makeFormLogin();
-        document.querySelector("#login").setAttribute("userid", "0");
-    }
-}
-
-function userIsLogged(data) {
-    return data.userid > 0;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.login = new Login();
+});
 
 function logout() {
-    let data = new FormData();
-    data.append('username', "");
-    data.append('password', "");
-    fetch('auth/checklogin.php', { method: "POST", body: data })
-        .then(response => response.json())
-        .then(data => treatLogin(data));
-}
-
-function makeFormLogin() {
-    document.querySelector("#login").innerHTML = `
-    <form action="javascript:login()" id="loginform">
-    <input type="text" size="6" name="username" placeholder="username" value="someuser">
-    <input type="password" size="6" name="password" placeholder="password" value="123123">
-        <input type="submit" value="login">
-    </form>`;
+    document.body.login.logout();
 }
 
 function login() {
-    const form = document.querySelector("#loginform");
-    let data = new FormData();
-    data.append('username', form.username.value);
-    data.append('password', form.password.value);
-    fetch('auth/checklogin.php', { method: 'POST', body: data })
-        .then(response => response.json())
-        .then(data => treatLogin(data));
+    document.body.login.login();
+}
+
+class Login {
+
+    constructor() {
+        this.makeDivLogin();
+    }
+
+    makeDivLogin() {
+        this.loginDiv = document.createElement("div");
+        this.loginDiv.setAttribute("id", "login");
+        document.body.appendChild(this.loginDiv);
+        this.fetchCheckLoginPage();
+    }
+
+    fetchCheckLoginPage() {
+        fetch('auth/checklogin.php')
+            .then(response => response.json())
+            .then(data => {
+                this.checkLogin(data);
+            });
+    }
+
+    checkLogin(user) {
+        if (user.userid > 0) {
+            this.makeFormLogout(user);
+            this.loginDiv.setAttribute("userid", user.userid);
+        } else {
+            this.makeFormLogin();
+            this.loginDiv.setAttribute("userid", "0");
+        }
+    }
+
+    makeFormLogout(user) {
+        this.loginDiv.innerHTML = `${user.username}
+        <a href="javascript:logout()">logout</a>`;
+    }
+
+    logout() {
+        let data = new FormData();
+        data.append('username', "");
+        data.append('password', "");
+        fetch('auth/checklogin.php', { method: "POST", body: data })
+            .then(response => response.json())
+            .then(data => {
+                this.checkLogin(data);
+            });
+    }
+
+    makeFormLogin() {
+        this.loginDiv.innerHTML = `
+        <form action="javascript:login()" id="loginform">
+            <input type="text" size="6" name="username" placeholder="username" value="someuser">
+            <input type="password" size="6" name="password" placeholder="password" value="123123">
+            <input type="submit" value="login">
+        </form>`;
+    }
+
+    login() {
+        const form = this.loginDiv.querySelector("form");
+        let data = new FormData();
+        data.append('username', form.username.value);
+        data.append('password', form.password.value);
+        fetch('auth/checklogin.php', { method: 'POST', body: data })
+            .then(response => response.json())
+            .then(data => {
+                this.checkLogin(data);
+            });
+    }
+
 }
